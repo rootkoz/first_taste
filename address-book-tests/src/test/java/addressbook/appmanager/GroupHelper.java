41cd5c4f5d364834ecd1bd2945859f189c497411
rootkoz <ihateciti@gmail.com>
2016-03-03 01:03:12 +0400

    HelperBase

--- address-book-tests/src/test/java/addressbook/appmanager/GroupHelper.java
+++ address-book-tests/src/test/java/addressbook/appmanager/GroupHelper.java
@@ -10,54 +10,38 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  */
 
 
-public class GroupHelper {
-    private FirefoxDriver wd;
+public class GroupHelper extends HelperBase{
 
     public GroupHelper(FirefoxDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void fillGroupForm(GroupData groupData) {
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-        if (!wd.findElement(By.xpath("//div[@id='content']/form/select//option[1]")).isSelected()) {
-            wd.findElement(By.xpath("//div[@id='content']/form/select//option[1]")).click();
-        }
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-        wd.findElement(By.name("group_footer")).click();
-        wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+        type(By.name("group_name"), groupData.getName());
+        type(By.name("group_header"), groupData.getHeader());
+        type(By.name("group_footer"), groupData.getFooter());
     }
 
     public void submitGroupCreation() {
-        wd.findElement(By.name("submit")).click();
+        click(By.name("submit"));
     }
 
     public void createNewGroup() {
-        wd.findElement(By.name("new")).click();
+        click(By.name("new"));
     }
 
     public void deleteSelectedGroup() {
-        wd.findElement(By.xpath("//div[@id='content']/form/input[5]")).click();
+        click(By.xpath("//div[@id='content']/form/input[5]"));
     }
 
     public void selectGroup() {
-        wd.findElement(By.name("selected[]")).click();
+        click(By.name("selected[]"));
     }
     public void editGroup(String header, String footer, String groupname) {
-        wd.findElement(By.name("edit")).click();
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys(header);
-        wd.findElement(By.name("group_footer")).click();
-        wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys(footer);
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys(groupname);
-        wd.findElement(By.name("update")).click();
+        click(By.name("edit"));
+        type(By.name("group_header"), header);
+        type(By.name("group_footer"), footer);
+        type(By.name("group_name"), groupname);
+        click(By.name("update"));
     }
 }
---
+++ address-book-tests/src/test/java/addressbook/appmanager/HelperBase.java
@@ -0,0 +1,29 @@
+package addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/***
+ * by rootkoz
+ * >(((*>
+ */
+
+
+public class HelperBase {
+
+    protected FirefoxDriver wd;
+
+    public HelperBase(FirefoxDriver wd) {
+        this.wd = wd;
+    }
+
+    protected void type(By locator, String text) {
+        click(locator);
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+    }
+
+    protected void click(By locator) {
+        wd.findElement(locator).click();
+    }
+}
