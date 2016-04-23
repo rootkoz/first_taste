19b052dfc272759e383e01dd4a30d00ce3f37abf
rootkoz <ihateciti@gmail.com>
2016-04-23 11:45:23 +0400

    back Action

--- sandbox/src/test/java/PointTests.java
+++ sandbox/src/test/java/PointTests.java
@@ -14,7 +14,7 @@ public class PointTests {
         Point p2 = new Point(3,4);
         Point p3 = new Point(0,8);
 
-        Assert.assertEquals(p1.distance(p1,p2),4.0);
+        Assert.assertEquals(p1.distance(p1,p2),5.0);
         Assert.assertEquals(p1.distance(p1,p3),8.0);
     }
 }
